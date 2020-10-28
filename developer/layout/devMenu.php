7d305dfade0a80ec7c54deee46a621ea79ba16d0
Studiodews <67610658+Studiodews@users.noreply.github.com>
2020-10-28 18:36:47 +0100

    Add files via upload

---
+++ developer/layout/devMenu.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * admin navigation menu 
+ * 
+ * @author mohamed hussein 
+ * @copyright 2017
+ */
+ ?>
+ <ul class="list-group">
+ <li><a href="./index.php" class="list-group-item">Dashboard</a></li>
+ <li><a href="./newApp.php" class="list-group-item">new App</a></li>
+ <li><a href="./devApps.php" class="list-group-item">apps list</a></li>
+ <li><a href="./devReviews.php" class="list-group-item">Reviews</a></li>
+ </ul>
