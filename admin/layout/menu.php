8fc56cae4ac568682dc41758484ad2742143a32e
Studiodews <67610658+Studiodews@users.noreply.github.com>
2020-10-28 18:32:17 +0100

    Add files via upload

---
+++ admin/layout/menu.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * admin navigation menu 
+ * 
+ * @author mohamed hussein 
+ * @copyright 2017
+ */
+ ?>
+ <ul id="adminMenu">
+ <li><a href="./index.php" id="hrefBtn">Dashboard</a></li>
+ <li><a href="./user.php" id="hrefBtn">Users</a></li>
+ <li><a href="./developers.php" id="hrefBtn">Developers</a></li>
+ <li><a href="./category.php" id="hrefBtn">Categories</a></li>
+ <li><a href="./platform.php" id="hrefBtn">Platforms</a></li>
+  <li><a href="./apps.php" id="hrefBtn">Apps</a></li>
+ <li><a href="./reviews.php" id="hrefBtn">Reviews</a></li>
+ </ul>
